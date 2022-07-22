e1b6c726641ed625dad573ae2beaf4de31ee1af2
Devin Carter <devinacarter97@gmail.com>
2022-07-22 12:04:31 +0100

    Finalised rock, paper, scissors app

--- rpc_func.js
+++ rpc_func.js
@@ -10,8 +10,7 @@ function computerPlay() {
 }
 
 //Plays a single round and returns a winner
-function singleRound() {
-    let usr = prompt("Choose from rock, paper or scissors");
+function singleRound(usr) {
 
     usr = usr.charAt(0).toUpperCase() + usr.slice(1);
 
@@ -28,14 +27,14 @@ function singleRound() {
         usr.toLowerCase() === 'scissors' &&
         compChoice.toLowerCase() === 'rock'
     ) {
-        return `You lose! ${compChoice} beats ${usr}`;
+        return `You Lose! ${compChoice} beats ${usr}`;
     } else {
         return `You Win! ${usr} beats ${compChoice}`;
     }
 }
 
 //Game is designed to run for 5 rounds and declare a winner at the end
-function game() {
+/** function game() {
     usrScore = 0;
     compScore = 0;
     //for loop goes over each round and adds 1 to the winner
@@ -60,4 +59,104 @@ function game() {
     } else {
         return "You loose!";
     }
+}
+**/
+
+const userscore = document.querySelector('#usr_score');
+
+const compscore = document.querySelector('#robot_score');
+
+const winner = document.querySelector('#winner');
+
+const usrquest = document.querySelector('#usrquest');
+
+const header = document.querySelector("h4");
+
+const rock = document.querySelector('#rock');
+
+const paper = document.querySelector('#paper');
+
+const scissors = document.querySelector('#scissors');
+
+rock.addEventListener('click', function() {
+    if (singleRound('rock').includes("You Win!")) {
+        if (userscore.value < 5 && compscore.value < 5) {
+            userscore.value++;
+            winner.textContent = 'You won that round!';
+        }
+    } else if (singleRound('rock').includes("You Lose!")) {
+        if (userscore.value < 5 && compscore.value < 5) {
+            compscore.value++;
+            winner.textContent = 'Dang! You lost that round';
+        }
+    } else {
+        if (userscore.value < 5 && compscore.value < 5) {
+            winner.textContent = 'This round was a tie!';
+        }
+    }
+    winnerfunc();
+});
+
+
+paper.addEventListener('click', function() {
+    if (singleRound('paper').includes("You Win!")) {
+        if (userscore.value < 5 && compscore.value < 5) {
+            userscore.value++;
+            winner.textContent = 'You won that round!';
+        }
+    } else if (singleRound('paper').includes("You Lose!")) {
+        if (userscore.value < 5 && compscore.value < 5) {
+            compscore.value++;
+            winner.textContent = 'Dang! You lost that round';
+        }
+    } else {
+        if (userscore.value < 5 && compscore.value < 5) {
+            winner.textContent = 'This round was a tie!';
+        }
+    }
+    winnerfunc();
+});
+
+
+
+scissors.addEventListener('click', function() {
+    if (singleRound('scissors').includes("You Win!")) {
+        if (userscore.value < 5 && compscore.value < 5) {
+            userscore.value++;
+            winner.textContent = 'You won that round!';
+        }
+    } else if (singleRound('scissors').includes("You Lose!")) {
+        if (userscore.value < 5 && compscore.value < 5) {
+            compscore.value++;
+            winner.textContent = 'Dang! You lost that round';
+        }
+    } else {
+        if (userscore.value < 5 && compscore.value < 5) {
+            winner.textContent = 'This round was a tie!';
+        }
+    }
+    winnerfunc();
+});
+
+function winnerfunc() {
+    if (userscore.value == 5) {
+        winner.textContent = "You win!";
+        usrquest.textContent = "Play again?"
+    } else if (compscore.value == 5) {
+        winner.textContent = "Oh no! The computer won!";
+        usrquest.textContent = 'Play again?'
+
+    }
+}
+
+const playagain = document.querySelector('#playagain');
+playagain.addEventListener('click', function() {
+    reset();
+});
+
+function reset() {
+    compscore.value = 0;
+    userscore.value = 0;
+    winner.textContent = " ";
+    usrquest.textContent = " ";
 }
